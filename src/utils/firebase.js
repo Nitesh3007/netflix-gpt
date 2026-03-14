c0fbea54a35aeea93f4175bfbe1de38d6131815c
NiteshHL <niteshp.int@homelane.com>
2026-03-14 21:19:11 +0530

    add firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+ // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBzKaqETBbMpMQmmQb_d1t-5xNbq-gr8BA",
+  authDomain: "netflixgpt-001-5b3fd.firebaseapp.com",
+  projectId: "netflixgpt-001-5b3fd",
+  storageBucket: "netflixgpt-001-5b3fd.firebasestorage.app",
+  messagingSenderId: "1091469119232",
+  appId: "1:1091469119232:web:c7a426050ae4d68ae8ede5",
+  measurementId: "G-26QYEWXS8Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
